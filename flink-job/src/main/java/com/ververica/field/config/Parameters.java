b0634f03654b309bf94bc83f7d0b2482f122c1e5
Wangziren <wzr4548@gmail.com>
2025-01-09 17:55:55 +0800

    [refactor] change params config

--- flink-job/src/main/java/com/ververica/field/config/Parameters.java
+++ flink-job/src/main/java/com/ververica/field/config/Parameters.java
@@ -18,54 +18,59 @@
 
 package com.ververica.field.config;
 
+import org.apache.flink.api.java.utils.ParameterTool;
+
 import java.util.Arrays;
 import java.util.List;
-import org.apache.flink.api.java.utils.ParameterTool;
 
 public class Parameters {
 
-  private final ParameterTool tool;
+    private final ParameterTool tool;
 
-  public Parameters(ParameterTool tool) {
-    this.tool = tool;
-  }
-
-  <T> T getOrDefault(Param<T> param) {
-    if (!tool.has(param.getName())) {
-      return param.getDefaultValue();
+    public Parameters(ParameterTool tool) {
+        this.tool = tool;
     }
-    Object value;
-    if (param.getType() == Integer.class) {
-      value = tool.getInt(param.getName());
-    } else if (param.getType() == Long.class) {
-      value = tool.getLong(param.getName());
-    } else if (param.getType() == Double.class) {
-      value = tool.getDouble(param.getName());
-    } else if (param.getType() == Boolean.class) {
-      value = tool.getBoolean(param.getName());
-    } else {
-      value = tool.get(param.getName());
+
+    <T> T getOrDefault(Param<T> param) {
+        if (!tool.has(param.getName())) {
+            return param.getDefaultValue();
+        }
+        Object value;
+        if (param.getType() == Integer.class) {
+            value = tool.getInt(param.getName());
+        } else if (param.getType() == Long.class) {
+            value = tool.getLong(param.getName());
+        } else if (param.getType() == Double.class) {
+            value = tool.getDouble(param.getName());
+        } else if (param.getType() == Boolean.class) {
+            value = tool.getBoolean(param.getName());
+        } else {
+            value = tool.get(param.getName());
+        }
+        return param.getType().cast(value);
     }
-    return param.getType().cast(value);
-  }
 
-  public static Parameters fromArgs(String[] args) {
-    ParameterTool tool = ParameterTool.fromArgs(args);
-    return new Parameters(tool);
-  }
+    public static Parameters fromArgs(String[] args) {
+        ParameterTool tool = ParameterTool.fromArgs(args);
+        return new Parameters(tool);
+    }
 
-  // Kafka:
-  public static final Param<String> KAFKA_HOST = Param.string("kafka-host", "localhost");
-  public static final Param<Integer> KAFKA_PORT = Param.integer("kafka-port", 9092);
+    // Kafka:
+    public static final Param<String> KAFKA_HOST = Param.string("kafka-host", "localhost");
 
-  public static final Param<String> DATA_TOPIC = Param.string("data-topic", "livetransactions");
-  public static final Param<String> ALERTS_TOPIC = Param.string("alerts-topic", "alerts");
-  public static final Param<String> RULES_TOPIC = Param.string("rules-topic", "rules");
-  public static final Param<String> LATENCY_TOPIC = Param.string("latency-topic", "latency");
-  public static final Param<String> RULES_EXPORT_TOPIC =
-      Param.string("current-rules-topic", "current-rules");
+    public static final Param<Integer> KAFKA_PORT = Param.integer("kafka-port", 9092);
 
-  public static final Param<String> OFFSET = Param.string("offset", "latest");
+    public static final Param<String> DATA_TOPIC = Param.string("data-topic", "livetransactions");
+
+    public static final Param<String> ALERTS_TOPIC = Param.string("alerts-topic", "alerts");
+
+    public static final Param<String> RULES_TOPIC = Param.string("rules-topic", "rules");
+
+    public static final Param<String> LATENCY_TOPIC = Param.string("latency-topic", "latency");
+
+    public static final Param<String> RULES_EXPORT_TOPIC = Param.string("current-rules-topic", "current-rules");
+
+    public static final Param<String> OFFSET = Param.string("offset", "latest");
 
   // GCP PubSub:
   public static final Param<String> GCP_PROJECT_NAME = Param.string("gcp-project", "da-fe-212612");
@@ -89,19 +94,17 @@ public class Parameters {
   public static final Param<String> LATENCY_SINK = Param.string("latency-sink", "STDOUT");
   public static final Param<String> RULES_EXPORT_SINK = Param.string("rules-export-sink", "STDOUT");
 
-  public static final Param<Integer> RECORDS_PER_SECOND = Param.integer("records-per-second", 2);
+    public static final Param<Integer> RECORDS_PER_SECOND = Param.integer("records-per-second", 2);
 
-  public static final Param<Boolean> LOCAL_EXECUTION = Param.bool("local", false);
+    public static final Param<Boolean> LOCAL_EXECUTION = Param.bool("local", false);
 
-  public static final Param<Integer> SOURCE_PARALLELISM = Param.integer("source-parallelism", 2);
+    public static final Param<Integer> SOURCE_PARALLELISM = Param.integer("source-parallelism", 2);
 
-  public static final Param<Boolean> ENABLE_CHECKPOINTS = Param.bool("checkpoints", false);
+    public static final Param<Boolean> ENABLE_CHECKPOINTS = Param.bool("checkpoints", false);
 
-  public static final Param<Integer> CHECKPOINT_INTERVAL =
-      Param.integer("checkpoint-interval", 60_000_0);
-  public static final Param<Integer> MIN_PAUSE_BETWEEN_CHECKPOINTS =
-      Param.integer("min-pause-btwn-checkpoints", 10000);
-  public static final Param<Integer> OUT_OF_ORDERNESS = Param.integer("out-of-orderdness", 500);
+    public static final Param<Integer> CHECKPOINT_INTERVAL = Param.integer("checkpoint-interval", 60_000_0);
+    public static final Param<Integer> MIN_PAUSE_BETWEEN_CHECKPOINTS = Param.integer("min-pause-btwn-checkpoints", 10000);
+    public static final Param<Integer> OUT_OF_ORDERNESS = Param.integer("out-of-orderdness", 500);
 
   //  List<Param> list = Arrays.asList(new String[]{"foo", "bar"});
 
@@ -135,6 +138,8 @@ public class Parameters {
           MIN_PAUSE_BETWEEN_CHECKPOINTS,
           OUT_OF_ORDERNESS);
 
-  public static final List<Param<Boolean>> BOOL_PARAMS =
-      Arrays.asList(LOCAL_EXECUTION, ENABLE_CHECKPOINTS);
+    public static final List<Param<Boolean>> BOOL_PARAMS = Arrays.asList(
+            LOCAL_EXECUTION,
+            ENABLE_CHECKPOINTS
+    );
 }
--- flink-job/src/main/java/com/ververica/field/dynamicrules/Main.java
+++ flink-job/src/main/java/com/ververica/field/dynamicrules/Main.java
@@ -18,21 +18,18 @@
 
 package com.ververica.field.dynamicrules;
 
-import static com.ververica.field.config.Parameters.BOOL_PARAMS;
-import static com.ververica.field.config.Parameters.INT_PARAMS;
-import static com.ververica.field.config.Parameters.STRING_PARAMS;
-
 import com.ververica.field.config.Config;
 import com.ververica.field.config.Parameters;
 import org.apache.flink.api.java.utils.ParameterTool;
 
+
 public class Main {
 
-  public static void main(String[] args) throws Exception {
-    ParameterTool tool = ParameterTool.fromArgs(args);
-    Parameters inputParams = new Parameters(tool);
-    Config config = new Config(inputParams, STRING_PARAMS, INT_PARAMS, BOOL_PARAMS);
-    RulesEvaluator rulesEvaluator = new RulesEvaluator(config);
-    rulesEvaluator.run();
-  }
+    public static void main(String[] args) throws Exception {
+        ParameterTool tool = ParameterTool.fromArgs(args);
+        Parameters inputParams = new Parameters(tool);
+        Config config = Config.fromParameters(inputParams);
+        RulesEvaluator rulesEvaluator = new RulesEvaluator(config);
+        rulesEvaluator.run();
+    }
 }
